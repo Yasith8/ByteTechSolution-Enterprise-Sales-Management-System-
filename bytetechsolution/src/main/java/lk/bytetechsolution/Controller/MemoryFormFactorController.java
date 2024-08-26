b4eee3586eb545683f613578a056ef14f896f9f1
Yasith <yasithnawodya9@gmail.com>
2024-08-26 11:17:43 +0530

    create memoryformfactor  dao/entity/controller

--- bytetechsolution/src/main/java/lk/bytetechsolution/Controller/MemoryFormFactorController.java
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Controller/MemoryFormFactorController.java
@@ -4,8 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lk.bytetechsolution.Dao.MemoryTypeDao;
-import lk.bytetechsolution.Entity.MemoryTypeEntity;
+import lk.bytetechsolution.Dao.MemoryFormFactorDao;
+import lk.bytetechsolution.Entity.MemoryFormFactorEntity;
+
 import java.util.*;
 
 /*
@@ -15,26 +16,25 @@ import java.util.*;
     * restcontroller tell spingboot to class/methods responsible to handle incoming http requests and produce appropiate http response(using json,xml format)
 */
 @RestController
-public class MemoryTypeController {
-     /* 
+public class MemoryFormFactorController {
+    /* 
      * AutoWired used for automatic dependency injection
      * inject memorytype Instance into dao variable
      * the method can use dao for save,retrive,maipulate motherboardformfactor data
      */
     @Autowired
-    private MemoryTypeDao dao;
-
-      /* 
+    private MemoryFormFactorDao dao;
+     /* 
      * define mapping for get all GEN data from database
      * produce -> data return format(json,xml)
      * value or path -> specify url pattern to which the method will be mapped
      * List<cpuseries> -> return the list of Employee object
      * 
      * also you can use
-     * @requestMapping(value="/memorytype/alldata",produces='application.json',method=RequestMethod.GET)
+     * @requestMapping(value="/memoryformfactor/alldata",produces='application.json',method=RequestMethod.GET)
      */
-    @GetMapping(value = "/memorytype/alldata", produces ="application/json" ) 
-    public List<MemoryTypeEntity> allEmployeeData() {
+    @GetMapping(value = "/memoryformfactor/alldata", produces ="application/json" ) 
+    public List<MemoryFormFactorEntity> allEmployeeData() {
 
         return dao.findAll();
     }
---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Dao/MemoryFormFactorDao.java
@@ -0,0 +1,11 @@
+package lk.bytetechsolution.Dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import lk.bytetechsolution.Entity.MemoryFormFactorEntity;
+
+public interface MemoryFormFactorDao extends JpaRepository<MemoryFormFactorEntity, Integer> {
+
+
+    
+} 
