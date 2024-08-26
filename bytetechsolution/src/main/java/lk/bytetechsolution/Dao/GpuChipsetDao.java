e3e0f5d76378fc257af1b2882e4a10cc1d2202d2
Yasith <yasithnawodya9@gmail.com>
2024-08-26 11:24:11 +0530

    create gpuchipset  dao/entity/controller

--- bytetechsolution/src/main/java/lk/bytetechsolution/Dao/GpuChipsetDao.java
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Dao/GpuChipsetDao.java
@@ -2,15 +2,14 @@ package lk.bytetechsolution.Dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import lk.bytetechsolution.Entity.MemoryFormFactorEntity;
+import lk.bytetechsolution.Entity.GpuChipsetEntity;
 
 /* 
  * MemoryFormFactorDao extended from jparepository
  * MemoryFormFactorDao inherit all the methods provides by  JPARepository for CRUD operations on MemoryFormFactorEntity
  * Integer spesify the type of primary key in MemoryFormFactorEntity is Integer
  */
-public interface MemoryFormFactorDao extends JpaRepository<MemoryFormFactorEntity, Integer> {
-
+public interface GpuChipsetDao extends JpaRepository<GpuChipsetEntity,Integer>{
 
     
 } 
