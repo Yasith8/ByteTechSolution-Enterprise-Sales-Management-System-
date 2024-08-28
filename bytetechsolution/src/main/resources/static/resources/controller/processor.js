1462ac9141d037c20bbdd10b43140467d4e1cbf7
Yasith <yasithnawodya9@gmail.com>
2024-08-28 10:56:51 +0530

    create functions for get data to table in processor ui

---
+++ bytetechsolution/src/main/resources/static/resources/controller/processor.js
@@ -0,0 +1,72 @@
+window.addEventListener('load', () => {
+    refreshProcessorTable();
+    refreshProcessorForm();
+})
+
+const refreshProcessorTable = () => {
+
+    //get data using ajax request
+    processors = getServiceAjaxRequest("/processor/alldata");
+
+    const displayPropertyList = [
+        { dataType: 'text', propertyName: 'itemcode' },
+        { dataType: 'text', propertyName: 'itemname' },
+        { dataType: 'function', propertyName: getBrandName },
+        { dataType: 'text', propertyName: 'quantity' },
+        { dataType: 'text', propertyName: 'salesprice' },
+        { dataType: 'text', propertyName: 'purchaseprice' },
+        { dataType: 'function', propertyName: getCpuSeries },
+        { dataType: 'function', propertyName: getCpuGeneration },
+        { dataType: 'function', propertyName: getCpuSocket },
+        { dataType: 'function', propertyName: getItemStatus },
+    ]
+
+    fillDataIntoTable(tableProcessor, processors, displayPropertyList, refillItemForm, divModifyButton)
+        //table show with dataTable
+    $('#tableProcessor').dataTable();
+    //hide button section
+    divModifyButton.className = 'd-none';
+
+}
+
+const refreshProcessorForm = () => {
+
+}
+
+
+const getBrandName = (ob) => {
+    return ob.brand_id.name;
+}
+
+const getCpuSeries = (ob) => {
+    return ob.cpuseries_id.name;
+}
+
+const getCpuGeneration = (ob) => {
+    return ob.cpugeneration_id.name;
+}
+
+const getCpuSocket = (ob) => {
+    return ob.cpusocket_id.name;
+}
+
+const getItemStatus = (ob) => {
+    if (ob.itemstatus_id.name == 'Available') {
+        return '<p  class="status-active">' + ob.itemstatus_id.name + '</p>';
+    }
+
+    if (ob.itemstatus_id.name == 'Low-Stock') {
+        return '<p  class="status-resign">' + ob.itemstatus_id.name + '</p>'
+    }
+
+
+    if (ob.itemstatus_id.name == 'Unavailable') {
+        return '<p  class="itemstatus-delete">' + ob.itemstatus_id.name + '</p>'
+    } else {
+        return '<p  class="status-other">' + ob.itemstatus_id.name + '</p>'
+    }
+}
+
+const refillItemForm = () => {
+
+}
