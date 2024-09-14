70d1312075f8c6ad747624e236c0b085aaeaaa18
Yasith <yasithnawodya9@gmail.com>
2024-09-14 19:10:46 +0530

    create gpu delete function

--- bytetechsolution/src/main/resources/static/resources/controller/gpu.js
+++ bytetechsolution/src/main/resources/static/resources/controller/gpu.js
@@ -460,3 +460,39 @@ const buttonGpuUpdate = () => {
 
 
 }
+
+const deleteGpu = (ob, rowIndex) => {
+    //user conformation
+    let userConform = confirm("Are you sure  to delete following Gpu? " + ob.itemname);
+
+    //if ok
+    if (userConform) {
+        let deleteServiceResponse;
+
+        //ajax request fot delete data
+        $.ajax("/gpu", {
+            type: "DELETE",
+            contentType: "application/json",
+            data: JSON.stringify(ob),
+            async: false,
+
+            success: function(data) {
+                deleteServiceResponse = data
+            },
+
+            error: function(errData) {
+                deleteServiceResponse = errData;
+            }
+        })
+
+        //if delete response ok alert the success message and close the modal and refreash item table
+        //so because of that we can see realtime update
+        if (deleteServiceResponse == "OK") {
+            alert("Delete Successfullly");
+            $('#gpuAddModal').modal('hide');
+            refreshGpuTable()
+        } else {
+            console.log("system has following errors:\n" + deleteServiceResponse);
+        }
+    }
+}
