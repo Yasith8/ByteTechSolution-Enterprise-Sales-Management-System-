a78a0354e13e96732ff5a918090b37a58eb810f2
Yasith <yasithnawodya9@gmail.com>
2024-09-14 17:27:14 +0530

    create refill function for gpu

--- bytetechsolution/src/main/resources/static/resources/controller/gpu.js
+++ bytetechsolution/src/main/resources/static/resources/controller/gpu.js
@@ -113,7 +113,7 @@ const getGpuChipset = (ob) => {
 }
 
 const getGpuCapacity = (ob) => {
-    return ob.gpucapacity_id.name;
+    return ob.capacity_id.name;
 }
 
 const getItemStatus = (ob) => {
@@ -134,5 +134,93 @@ const getItemStatus = (ob) => {
 }
 
 const refillGpuForm = (ob, rowIndex) => {
+    $('#gpuAddModal').modal('show');
+    removeValidationColor([textItemName, decimalPurchasePrice, decimalSalesPrice, numberProfitRate, numberROP, numberROQ, numberWarranty, textDescription, selectMotherboardFormFactor, selectBrand, selectItemStatus, selectGpuType, selectInterface, selectCapacity, selectGpuChipset, selectGpuSeries])
+
+    buttonSubmit.disabled = true;
+    buttonSubmit.classList.remove('modal-btn-submit');
+
+    buttonUpdate.disabled = false;
+    buttonUpdate.classList.add('modal-btn-update');
+
+    gpu = JSON.parse(JSON.stringify(ob));
+    oldGpu = ob;
+
+    //asign itemcode
+    staticBackdropLabel.textContent = gpu.itemcode;
+
+    //assign item name
+    textItemName.value = gpu.itemname;
+    //assign purchase price
+    decimalPurchasePrice.value = gpu.purchaseprice;
+    //assign profit rate
+    numberProfitRate.value = gpu.profitrate;
+    //assign sales price
+    decimalSalesPrice.value = gpu.salesprice;
+    //assign rop 
+    numberROP.value = gpu.rop;
+    //assign roq 
+    numberROQ.value = gpu.roq;
+    //assign warranty
+    numberWarranty.value = gpu.warranty;
+    //asign description
+    textDescription.value = gpu.description;
+
+    //get brands of motherboard
+    brands = getServiceAjaxRequest("/brand/brandbycategory/Motherboard");
+    fillDataIntoSelect(selectBrand, "Please Select Brand", brands, "name", ob.brand_id.name);
+
+    itemstatuses = getServiceAjaxRequest("/itemstatus/alldata")
+    fillDataIntoSelect(selectItemStatus, "Please Select", itemstatuses, "name", ob.itemstatus_id.name);
+
+    motherboardformfactors = getServiceAjaxRequest("/motherboardformfactor/alldata")
+    fillDataIntoSelect(selectMotherboardFormFactor, "Please Select Motherboard Form Factor", motherboardformfactors, "name", ob.motherboardformfactor_id.name);
+
+    gputypes = getServiceAjaxRequest("/gputype/alldata")
+    fillDataIntoSelect(selectGpuType, "Please Select GPU Type", gputypes, "name", ob.gputype_id.name);
+
+    interfaces = getServiceAjaxRequest("/interface/alldata")
+    fillDataIntoSelect(selectInterface, "Please Select Interface", interfaces, "name", ob.interface_id.name);
+
+    capacities = getServiceAjaxRequest("/capacity/alldata")
+    fillDataIntoSelect(selectCapacity, "Please Select Capacity", capacities, "name", ob.capacity_id.name);
+
+    gpuchipsets = getServiceAjaxRequest("/gpuchipset/alldata")
+    fillDataIntoSelect(selectGpuChipset, "Please Select GPU Chipset", gpuchipsets, "name", ob.gpuchipset_id.name);
+
+    fillDataIntoSelect(selectGpuSeries, "Select GPU Series", [], "name", ob.gpuseries_id.name);
+
+    selectGpuChipset.addEventListener('change', () => {
+        const gpuchipset = selectValueHandler(selectGpuChipset);
+        gpuseries = getServiceAjaxRequest("/gpuseries/gpuseriesbygpuchipset/" + gpuchipset.name);
+        fillDataIntoSelect(selectGpuSeries, "Select GPU Series", gpuseries, "name");
+    })
+
+
+
+    inputFieldsHandler([textItemName, decimalPurchasePrice, decimalSalesPrice, numberProfitRate, numberROP, numberROQ, numberWarranty, textDescription, selectMotherboardFormFactor, selectBrand, selectItemStatus, selectGpuType, selectInterface, selectCapacity, selectGpuChipset, selectGpuSeries], false);
+    buttonClear.classList.add('modal-btn-clear');
+
+
+
+    let userPrivilage = getServiceAjaxRequest("/privilage/byloggeduser/GPU");
+    //console.log(userPrivilage);
+
+
+    if (!userPrivilage.update) {
+        buttonUpdate.disabled = true;
+        buttonUpdate.classList.remove('modal-btn-update');
+
+        inputFieldsHandler([textItemName, decimalPurchasePrice, decimalSalesPrice, numberProfitRate, numberROP, numberROQ, numberWarranty, textDescription, selectMotherboardFormFactor, selectBrand, selectItemStatus, selectGpuType, selectInterface, selectCapacity, selectGpuChipset, selectGpuSeries], true);
+        buttonClear.classList.remove('modal-btn-clear');
+    }
+    if (!userPrivilage.delete) {
+        buttonDelete.disabled = true;
+        buttonDelete.classList.remove('modal-btn-delete');
+    }
+
+
+    buttonClear.disabled = true;
+
 
 }
