03b908cdf4dcc8dc6e876236b65b9098e41ba331
Yasith <yasithnawodya9@gmail.com>
2024-09-19 20:42:09 +0530

    create onload function for memory js 2

--- bytetechsolution/src/main/resources/static/resources/controller/memory.js
+++ bytetechsolution/src/main/resources/static/resources/controller/memory.js
@@ -24,3 +24,34 @@ const refreshMemoryTable = () => {
 const refreshMemoryForm = () => {
 
 }
+
+
+
+const getBrandName = (ob) => {
+    return ob.brand_id.name
+}
+const getMemoryType = (ob) => {
+    return ob.memorytype_id.name
+}
+const getMemoryFormFactor = (ob) => {
+    return ob.memoryformfactor_id.name
+}
+const getMemoryCapacity = (ob) => {
+    return ob.capacity_id.name
+}
+const getItemStatus = (ob) => {
+    if (ob.itemstatus_id.name == 'Available') {
+        return '<p class="item-status-available">' + ob.itemstatus_id.name + '</p>';
+    }
+
+    if (ob.itemstatus_id.name == 'Low-Stock') {
+        return '<p class="item-status-resign">' + ob.itemstatus_id.name + '</p>'
+    }
+
+
+    if (ob.itemstatus_id.name == 'Unavailable') {
+        return '<p class="item-status-delete">' + ob.itemstatus_id.name + '</p>'
+    } else {
+        return '<p class="item-status-other">' + ob.itemstatus_id.name + '</p>'
+    }
+}
