e0e10310600e469a372c1cc6e6d8e96953c6942e
Yasith <yasithnawodya9@gmail.com>
2024-09-22 18:17:53 +0530

    create casing dao

---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Dao/CasingDao.java
@@ -0,0 +1,19 @@
+package lk.bytetechsolution.Dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import lk.bytetechsolution.Entity.CasingEntity;
+/* 
+ * CasingDao extended from jparepository
+ * CasingDao inherit all the methods provides by  JPARepository for CRUD operations on CasingEntity
+ * Integer spesify the type of primary key in CasingEntity is Integer
+ */
+public interface CasingDao extends JpaRepository<CasingEntity,Integer>{
+     @Query("select cas from CasingEntity cas where cas.itemname=?1")
+    public CasingEntity getByCaseName(String itemname);
+
+    @Query(value = "select concat('CAS',lpad(substring(max(cas.itemcode),4)+1,4,'0')) as itemcode from bytetechsolution.casing as cas",nativeQuery = true)
+    public String getNextCaseNumber();
+}
+
