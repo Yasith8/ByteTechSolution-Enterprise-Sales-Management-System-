83a6216d2d212cacb50f268fc072fc4c02409d11
Yasith <yasithnawodya9@gmail.com>
2024-09-26 21:01:15 +0530

    create storage refreash form (original)

--- bytetechsolution/src/main/resources/static/resources/controller/storage.js
+++ bytetechsolution/src/main/resources/static/resources/controller/storage.js
@@ -18,15 +18,53 @@ const refreshStorageTable = () => {
         { dataType: 'function', propertyName: getItemStatus },
     ]
 
-    fillDataIntoTable(tableGpu, gpus, displayPropertyList, refillGpuForm, divModifyButton)
+    fillDataIntoTable(tableStorage, storages, displayPropertyList, refillStorageForm, divModifyButton)
         //table show with dataTable
-    $('#tableGpu').dataTable();
+    $('#tableStorage').dataTable();
     //hide button section
     divModifyButton.className = 'd-none';
 
 }
 
 const refreshStorageForm = () => {
+    storage = new Object();
+
+    buttonSubmit.disabled = false;
+    buttonSubmit.classList.add('modal-btn-submit');
+
+    buttonUpdate.disabled = true;
+    buttonUpdate.classList.remove('modal-btn-update');
+
+    staticBackdropLabel.textContent = "Add New Item";
+
+    brands = getServiceAjaxRequest("/brand/brandbycategory/STORAGE");
+    fillDataIntoSelect(selectBrand, "Please Select Brand", brands, "name");
+
+    itemstatuses = getServiceAjaxRequest("/itemstatus/alldata")
+    fillDataIntoSelect(selectItemStatus, "Please Select Item Status", itemstatuses, "name");
+
+    storageInterfaces = getServiceAjaxRequest("/storageinterface/alldata")
+    fillDataIntoSelect(selectStorageInterfaces, "Please Select Storage Interface", storageInterfaces, "name");
+
+    storagetypes = getServiceAjaxRequest("/storagetype/alldata")
+    fillDataIntoSelect(selectStorageType, "Please Select Storage Type", storagetypes, "name");
+
+
+    capacities = getServiceAjaxRequest("/capacity/alldata")
+    fillDataIntoSelect(selectCapacity, "Please Select Capacity", capacities, "name");
+
+
+    removeValidationColor([textItemName, decimalPurchasePrice, decimalSalesPrice, numberProfitRate, numberROP, numberROQ, numberWarranty, textDescription, selectBrand, selectItemStatus, selectStorageType, selectStorageInterface, selectCapacity])
+
+    let userPrivilages = getServiceAjaxRequest("/privilage/byloggeduser/STORAGE");
+
+    if (!userPrivilages.insert) {
+        buttonSubmit.disabled = true;
+        buttonSubmit.classList.remove('modal-btn-submit');
+
+        inputFieldsHandler([textItemName, decimalPurchasePrice, decimalSalesPrice, numberProfitRate, numberROP, numberROQ, numberWarranty, textDescription, selectBrand, selectItemStatus, selectStorageType, selectStorageInterface, selectCapacity], true);
+        buttonClear.classList.remove('modal-btn-clear');
+    }
 
 }
 
@@ -57,4 +95,8 @@ const getItemStatus = (ob) => {
     } else {
         return '<p class="item-status-other">' + ob.itemstatus_id.name + '</p>'
     }
+}
+
+const refillStorageForm = (ob, rowIndex) => {
+
 }
