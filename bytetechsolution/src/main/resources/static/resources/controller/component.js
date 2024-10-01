9382b464c2931771ba37413e1ed4eb2b28b4d76d
Yasith <yasithnawodya9@gmail.com>
2024-10-01 19:39:29 +0530

    fix sidebar data loading issue

--- bytetechsolution/src/main/resources/static/resources/controller/component.js
+++ bytetechsolution/src/main/resources/static/resources/controller/component.js
@@ -1,15 +1,26 @@
 const navigatorList = [
-    { id: 1, labelName: 'Employee Management', linkUrl: '/employee', icon: '<i class="fas fa-users text-white"/>' },
-    { id: 2, labelName: 'User Management', linkUrl: '/user', icon: '<i class="fas fa-user text-white"/>' }
-]
+    { id: 1, labelName: 'Employee Management', linkUrl: '/employee', icon: '<i class="fas fa-users text-white"></i>' },
+    { id: 2, labelName: 'User Management', linkUrl: '/user', icon: '<i class="fas fa-user text-white"></i>' }
+];
 
-console.log("hi")
-const sidebarUL = document.getElementById('sidebarUL');
 
-sidebarUL.textContent = navigatorList.map((item) => {
-    return `
-        <button class="sidebar-btn" onclick="window.location.href='${item.linkUrl}'">
-            ${item.icon} ${item.labelName}
-        </button>
-    `;
-})
+document.addEventListener("DOMContentLoaded", function() {
+
+
+
+    let accessedModules = getServiceAjaxRequest("/module/listbyloggeduser")
+
+    // Ensure sidebarUL exists before appending
+    const sidebarUL = document.getElementById('sidebarUL');
+    if (sidebarUL && accessedModules) {
+        accessedModules.forEach(element => {
+            let module = element.toLowerCase();
+            let href = document.createElement('a');
+            href.innerText = element;
+            href.href = `/${module}`;
+            sidebarUL.appendChild(href);
+        });
+    } else {
+        console.error("SidebarUL element or accessedModules data not found");
+    }
+});
