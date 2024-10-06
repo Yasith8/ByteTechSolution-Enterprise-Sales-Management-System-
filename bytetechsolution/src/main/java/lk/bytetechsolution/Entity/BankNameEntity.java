c3bf37fc1b19c70ecb4ccbf013ddef1154747686
Yasith <yasithnawodya9@gmail.com>
2024-10-06 21:02:38 +0530

    create bank name entity

---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Entity/BankNameEntity.java
@@ -0,0 +1,31 @@
+package lk.bytetechsolution.Entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "bankname")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankNameEntity {
+      
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id",unique = true)
+    private int id;
+
+
+    @Column(name="name")
+    @NotNull
+    private String name;
+}
