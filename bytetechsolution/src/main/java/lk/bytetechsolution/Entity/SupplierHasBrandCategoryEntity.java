4c83b3e5717d962f1a3b76ca73c4621b494a3e96
Yasith <yasithnawodya9@gmail.com>
2024-10-08 20:45:34 +0530

    finishing up the initial supplier ui

---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Entity/SupplierHasBrandCategoryEntity.java
@@ -0,0 +1,40 @@
+package lk.bytetechsolution.Entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.*;
+
+@Entity
+@Table(name = "supplier_has_brand_category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SupplierHasBrandCategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",unique = true)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="supplier_id",referencedColumnName = "id")
+    private SupplierEntity supplier_id;
+
+    @ManyToOne
+    @JoinColumn(name="brand_id",referencedColumnName = "id")
+    private BrandEntity brand_id;
+    
+    @ManyToOne
+    @JoinColumn(name="category_id",referencedColumnName = "id")
+    private CategoryEntity category_id;
+}
