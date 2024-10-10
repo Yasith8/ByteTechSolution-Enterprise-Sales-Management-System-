571c1aa4b49b26de018edf5f02610edb8aa5dfb7
Yasith <yasithnawodya9@gmail.com>
2024-10-10 06:04:04 +0530

    add privilage security to supplier refeash form

--- bytetechsolution/src/main/resources/static/resources/controller/supplier.js
+++ bytetechsolution/src/main/resources/static/resources/controller/supplier.js
@@ -8,9 +8,36 @@ const refreshSupplierTable = () => {
 }
 
 const refreshSupplierForm = () => {
+    supplier = new Object();
+
+    //made security privilages
+    let userPrivilages = getServiceAjaxRequest("/privilage/byloggeduser/SUPPLIER");
+
+    if (!userPrivilages.insert) {
+        buttonSubmit.disabled = true;
+        buttonSubmit.classList.remove('modal-btn-submit');
+
+        inputFieldsHandler([textSupplierName, numberSupplierPhone, textSupplierEmail, textAddress, textAgentrName, numberAgentPhone, textAgentEmail, selectBankName, textBranch, textAccountName, numberAccountNo, selectSupplierStatus], true);
+        buttonClear.classList.remove('modal-btn-clear');
+    }
+
+
     refreshSupplierInnerFormAndTable();
 }
 
 const refreshSupplierInnerFormAndTable = () => {
+
+    //inner form
+    categories = getServiceAjaxRequest('category/alldata')
+    fillDataIntoSelect(selectInnerCategory, "Please Select Category", categories, "name");
+
+    fillDataIntoSelect(selectInnerBrand, "Please Select Category First", [], "name");
+
+    selectInnerCategory.addEventListener('onchange', () => {
+        let category = selectInnerCategory.value;
+
+        brands = getServiceAjaxRequest("/brand/brandbycategory/" + category);
+        fillDataIntoSelect(selectInnerBrand, "Please Select Brand", brands, "name");
+    })
 
 }
