fb3af3105ed4485d88bdbf2cb5d68361059e8fbd
Yasith <yasithnawodya9@gmail.com>
2024-10-21 19:23:10 +0530

    purchase requerst entity create

---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Entity/PurchaseRequestEntity.java
@@ -0,0 +1,48 @@
+package lk.bytetechsolution.Entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/* 
+ * ------------Entity-----------
+ * convert into entity class
+ * Entity from JPA-Java Persistence API(also known as jakarta persistence api)
+ * Entity used to mark class as a entity
+ * java tell system to instance of this class will represent rows in db's table
+ */
+@Entity
+@Table(name = "purchase_request") // map the modularity table
+@Data//genarate getters and setters and toString to class
+@AllArgsConstructor//default constructor       ----usefull when create object that not have any initial values
+@NoArgsConstructor//all argument constructor  ----usefull when create object that have any initial values
+public class PurchaseRequestEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //set Auto Increment
+    @Column(name = "id",unique = true)
+    private int id;
+
+    @Column(name = "requestcode",unique = true)
+    @NotNull
+    private String requestcode;
+
+    @Column(name = "totalamount")
+    @NotNull
+    private Integer totalamount;
+
+    @Column(name = "requireddate")
+    @NotNull
+    private LocalDateTime requireddate;
+
+}
