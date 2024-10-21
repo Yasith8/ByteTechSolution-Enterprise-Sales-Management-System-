caff30e2a0a8bee956c56a7c0f83d77b1c337308
Yasith <yasithnawodya9@gmail.com>
2024-10-21 20:54:55 +0530

    purchase status dao creation

---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Dao/PurchaseStatusDao.java
@@ -0,0 +1,10 @@
+package lk.bytetechsolution.Dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import lk.bytetechsolution.Entity.PurchaseStatusEntity;
+
+public interface PurchaseStatusDao extends JpaRepository<PurchaseStatusEntity,Integer>{
+
+    
+} 
