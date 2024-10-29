5bfe5145dd37420ce68ec68b023e69d493c6d120
Yasith <yasithnawodya9@gmail.com>
2024-10-29 19:32:05 +0530

    add grnitem entity

---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Entity/GRNItemEntity.java
@@ -0,0 +1,56 @@
+package lk.bytetechsolution.Entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity //class convert into entity. in here java tell to system instance of this class represent rows in table in db
+@Table(name = "grn_item")//assign the table to the entity class. here map to the table
+@Data// this data use to genarate getters and setters and toString to this class
+@AllArgsConstructor //AllArgsConstructor annotation generates a constructor initializing all object fields.
+@NoArgsConstructor //NoArgsConstructor generates a no-argument constructor for a class
+public class GRNItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //set Auto Increment
+    @Column(name = "id",unique = true)
+    private int id;
+
+    @NotNull
+    @Column(name = "itemcode")
+    private String itemcode;
+    
+    @NotNull
+    @Column(name = "itemname")
+    private String itemname;
+
+    @NotNull
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @NotNull
+    @Column(name = "purchaseprice")
+    private BigDecimal purchaseprice;
+
+    @NotNull
+    @Column(name = "lineprice")
+    private BigDecimal lineprice;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id",referencedColumnName = "id")
+    private CategoryEntity category_id;
+
+    @ManyToOne
+    @JoinColumn(name = "grn_id",referencedColumnName = "id")
+    private GRNEntity grn_id;
+}
