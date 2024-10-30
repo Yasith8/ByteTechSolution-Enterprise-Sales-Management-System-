b3e43a4bb1b7c8e087d67e7e16bc2be5df82b8e7
Yasith <yasithnawodya9@gmail.com>
2024-10-30 19:42:22 +0530

    add payment type and supplier payment entity

---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Entity/PaymentTypeEntity.java
@@ -0,0 +1,35 @@
+package lk.bytetechsolution.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/* 
+ * ------------Entity-----------
+ * convert into entity class
+ * Entity from JPA-Java Persistence API(also known as jakarta persistence api)
+ * Entity used to mark class as a entity
+ * java tell system to instance of this class will represent rows in db's table
+ */
+@Entity
+@Table(name = "payment_type") // map the modularity table
+@Data//genarate getters and setters and toString to class
+@AllArgsConstructor//AllArgsConstructor annotation generates a constructor initializing all object fields.
+@NoArgsConstructor//NoArgsConstructor generates a no-argument constructor for a class
+public class PaymentTypeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //set Auto Increment
+    @Column(name = "id",unique = true)
+    private int id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+}
---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Entity/SupplierPaymentEntity.java
@@ -0,0 +1,76 @@
+package lk.bytetechsolution.Entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/* 
+ * ------------Entity-----------
+ * convert into entity class
+ * Entity from JPA-Java Persistence API(also known as jakarta persistence api)
+ * Entity used to mark class as a entity
+ * java tell system to instance of this class will represent rows in db's table
+ */
+@Entity
+@Table(name = "supplier_payment") // map the modularity table
+@Data//genarate getters and setters and toString to class
+@AllArgsConstructor//AllArgsConstructor annotation generates a constructor initializing all object fields.
+@NoArgsConstructor//NoArgsConstructor generates a no-argument constructor for a class
+public class SupplierPaymentEntity {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //set Auto Increment
+    @Column(name = "id",unique = true)
+    private int id;
+
+    @NotNull
+    @Column(name = "paymentno")
+    private String paymentno;
+
+    @NotNull
+    @Column(name = "totalamount")
+    private BigDecimal totalamount;
+
+    @NotNull
+    @Column(name = "payedamount")
+    private BigDecimal payedamount;
+
+    @NotNull
+    @Column(name = "newdueamount")
+    private BigDecimal newdueamount;
+
+    @Column(name = "checkno")
+    private BigDecimal checkno;
+
+    @Column(name = "checkdate")
+    private LocalDateTime checkdate;
+
+    @Column(name = "transferno")
+    private String transferno;
+
+    @Column(name = "transferdatetime")
+    private LocalDateTime transferdatetime;
+
+    @ManyToOne
+    @JoinColumn(name = "grn_id",referencedColumnName = "id")
+    private GRNEntity grn_id;
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_id",referencedColumnName = "id")
+    private SupplierEntity supplier_id;
+
+    @ManyToOne
+    @JoinColumn(name = "payment_type_id",referencedColumnName = "id")
+    private PaymentTypeEntity payment_type_id;
+}
