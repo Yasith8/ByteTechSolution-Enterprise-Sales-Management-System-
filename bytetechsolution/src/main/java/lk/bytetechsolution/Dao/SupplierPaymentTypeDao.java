eb0cd7e01a81c0a50d2c2e9fc3a54d331fdeeb5b
Yasith <yasithnawodya9@gmail.com>
2024-11-01 21:41:35 +0530

    add supplier payement type dao

---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Dao/SupplierPaymentTypeDao.java
@@ -0,0 +1,17 @@
+package lk.bytetechsolution.Dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import lk.bytetechsolution.Entity.PaymentTypeEntity;
+
+/* 
+ * This dao file extends from JpaRepositoy
+ * JPA Repository CRUD and Pagination operations
+ * crud save(),delete(),findAll(),findById()
+ * pagination findAl()
+ * can use for custom query methods
+ */
+
+public interface SupplierPaymentTypeDao extends JpaRepository<PaymentTypeEntity,Integer> {
+    
+}
