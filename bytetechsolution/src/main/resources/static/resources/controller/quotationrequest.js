932083f6f188b994ca1892d6111afc2cef166df6
Yasith <yasithnawodya9@gmail.com>
2024-11-16 14:19:00 +0530

    Updated quotation request JavaScript file and HTML template with new function declarations and button event handlers.

---
+++ bytetechsolution/src/main/resources/static/resources/controller/quotationrequest.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', () => {
+    refreshQuotationRequestTable();
+    refreshQuotationRequestForm();
+})
+
+const refreshQuotationRequestTable = () => {
+
+}
+const refreshQuotationRequestForm = () => {
+
+}
+
+const refillQuotationRequestForm = () => {
+
+}
+
+const btnAddOneSupplier = () => {
+
+}
+const btnAddAllSupplier = () => {
+
+}
+
+const btnRemoveOneSupplier = () => {
+
+}
+
+
+const btnRemoveAllSupplier = () => {
+
+}
+
+
+const checkQuotationRequestInputErrors = () => {
+
+}
+
+const QuotationRequestHandler = () => {
+
+}
+
+const deleteQuotationRequest = () => {
+
+}
+
+const closeQRequestModal = () => {
+
+}
