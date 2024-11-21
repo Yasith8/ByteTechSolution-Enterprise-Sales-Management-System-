f6d4da933697876e3ea89ca0051fee70eb39b49d
Yasith <yasithnawodya9@gmail.com>
2024-11-21 10:03:13 +0530

    add request mapping to supplierquotation

--- bytetechsolution/src/main/java/lk/bytetechsolution/Controller/SupplierQuotationController.java
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Controller/SupplierQuotationController.java
@@ -8,13 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import lk.bytetechsolution.Dao.EmployeeDao;
 import lk.bytetechsolution.Dao.QuotationStatusDao;
 import lk.bytetechsolution.Dao.UserDao;
 import lk.bytetechsolution.Entity.GpuEntity;
 import lk.bytetechsolution.Entity.SupplierQuotationEntity;
+import lk.bytetechsolution.Entity.UserEntity;
 
 @RestController
 public class SupplierQuotationController {
@@ -32,6 +35,33 @@ public class SupplierQuotationController {
 
     @Autowired
     private PrivilageController privilageController;
+
+    @RequestMapping(value = "/supplierquotation")
+    public ModelAndView supplierQuotationUI(){
+         //get logged user authentication object using security
+        // this help to retrieve the current authentication object which holds the user detail
+        Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
+
+        //get current log user
+        UserEntity loggedUser=daoUser.getByUsername(authentication.getName());
+
+        //current loggedemployee
+        String loggedEmployee=daoEmployee.getFullnameById(loggedUser.getId());
+
+        // Create a new ModelAndView object to hold the model data and view information
+        ModelAndView supplierQuotationView=new ModelAndView();
+        //pass the ui
+        supplierQuotationView.setViewName("supplierquotation.html");
+        //attributes set to show titles in web page using theamleaf
+        supplierQuotationView.addObject("title", "Supplier Quotation Management || Bytetech Solution");
+        supplierQuotationView.addObject("user", authentication.getName());// passing logged user name
+        supplierQuotationView.addObject("EmpName", loggedEmployee);
+        supplierQuotationView.addObject("UserRole", loggedUser.getRoles().iterator().next().getName());//get the first role
+        supplierQuotationView.addObject("LoggedUserPhoto", loggedUser.getPhoto());
+
+        return supplierQuotationView;
+
+    }
 
     @GetMapping(value = "/supplierquotation/alldata",produces = "application/json")
     public List<SupplierQuotationEntity> getSupplierQuotationData(){
