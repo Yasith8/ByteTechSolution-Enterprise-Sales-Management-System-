e8cd12dc8525aa617a71253d0731dbe7695879ca
Yasith <yasithnawodya9@gmail.com>
2024-12-03 13:45:57 +0530

    add quotation item entity

---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Entity/QuotationItemEntity.java
@@ -0,0 +1,61 @@
+package lk.bytetechsolution.Entity;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity//class convert into entity. in here java tell to system instance of this class represent rows in table in db
+@Table(name = "quotation_item")//assign the table to the entity class. here map to the table
+@Data// this data use to genarate getters and setters and toString to this class
+@NoArgsConstructor//AllArgsConstructor annotation generates a constructor initializing all object fields.
+@AllArgsConstructor//NoArgsConstructor generates a no-argument constructor for a class
+public class QuotationItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //set Auto Increment
+    @Column(name = "id",unique = true)
+    private int id;
+
+    @NotNull
+    @Column(name = "itemcode")
+    private String itemcode;
+
+    @NotNull
+    @Column(name = "itemname")
+    private String itemname;
+
+    @NotNull
+    @Column(name = "unitprice")
+    private BigDecimal unitprice;
+    
+    @NotNull
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @NotNull
+    @Column(name = "lineprice")
+    private BigDecimal lineprice;
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_quotation_id",referencedColumnName = "id")
+    @JsonIgnore
+    private SupplierQuotationEntity supplier_quotation_id;
+
+}
