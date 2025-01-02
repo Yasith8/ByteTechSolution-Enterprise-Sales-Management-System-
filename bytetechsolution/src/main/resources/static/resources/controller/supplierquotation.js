b71386c5dbb3e6ed35c5137a15b1d6f288667c69
Yasith <yasithnawodya9@gmail.com>
2025-01-02 19:55:37 +0530

    enhance quotation status display with additional status styles

--- bytetechsolution/src/main/resources/static/resources/controller/supplierquotation.js
+++ bytetechsolution/src/main/resources/static/resources/controller/supplierquotation.js
@@ -35,7 +35,24 @@ const getSupplierName = (ob) => {
     return ob.quotation_request_id.supplier_id.name;
 }
 const getQuotationStatus = (ob) => {
+    if (ob.quotationstatus_id.name == 'Requested') {
+        return '<p class="quotation-requested">' + ob.quotationstatus_id.name + '</p>';
+    }
 
+    if (ob.quotationstatus_id.name == 'Canceled') {
+        return '<p class="quotation-canceled">' + ob.quotationstatus_id.name + '</p>'
+    }
+    if (ob.quotationstatus_id.name == 'Deleted') {
+        return '<p class="quotation-deleted">' + ob.quotationstatus_id.name + '</p>'
+    }
+    if (ob.quotationstatus_id.name == 'Accepted') {
+        return '<p class="quotation-accepted">' + ob.quotationstatus_id.name + '</p>'
+    }
+    if (ob.quotationstatus_id.name == 'Denied') {
+        return '<p class="quotation-denied">' + ob.quotationstatus_id.name + '</p>'
+    } else {
+        return '<p class="quotation-other">' + ob.quotationstatus_id.name + '</p>'
+    }
 }
 
 const refillSupplierForm = (ob) => {
