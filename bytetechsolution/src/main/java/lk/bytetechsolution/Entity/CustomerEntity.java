d4262b17ba7f34935cf4dba457910624799b8af1
Yasith <yasithnawodya9@gmail.com>
2025-01-11 20:21:15 +0530

    create customer entity

---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Entity/CustomerEntity.java
@@ -0,0 +1,79 @@
+package lk.bytetechsolution.Entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.*;
+
+/* 
+ * ------------Entity-----------
+ * convert into entity class
+ * Entity from JPA-Java Persistence API(also known as jakarta persistence api)
+ * Entity used to mark class as a entity
+ * java tell system to instance of this class will represent rows in db's table
+ */
+@Entity
+@Table(name = "customer") // map the modularity table
+@Data//genarate getters and setters and toString to class
+@AllArgsConstructor//default constructor       ----usefull when create object that not have any initial values
+@NoArgsConstructor//all argument constructor  ----usefull when create object that have any initial values
+public class CustomerEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //set Auto Increment
+    @Column(name = "id",unique = true)
+    private int id;
+
+    @Column(name = "name",unique = true)
+    @NotNull
+    private String name;
+
+    @Column(name = "mobile",unique = true)
+    @NotNull
+    private String mobile;
+
+    @Column(name = "email",unique = true)
+    @NotNull
+    private String email;
+
+    @Column(name = "address",unique = true)
+    private String address;
+
+    @Column(name = "totalpurchase",unique = true)
+    @NotNull
+    private BigDecimal totalpurchase;
+
+    @Column(name="addeddate")
+    @NotNull
+    private LocalDateTime addeddate;
+
+    @Column(name="modifydate")
+    private LocalDateTime modifydate;
+
+    @Column(name="deletedate")
+    private LocalDateTime deletedate;
+
+    
+    @Column(name = "addeduser")
+    @NotNull
+    private int addeduser;
+
+    @Column(name = "modifyuser")
+    private int modifyuser;
+    
+    @Column(name = "deleteuser")
+    private int deleteuser;
+
+    //todo customer statusmapping needed to add
+}
