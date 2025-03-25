aa3aedf3c1b787c4a074807a0822536a0805d3da
Yasith <yasithnawodya9@gmail.com>
2025-03-25 19:49:56 +0530

    create the suffix controller

---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Controller/CpuSuffixController.java
@@ -0,0 +1,60 @@
+package lk.bytetechsolution.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import lk.bytetechsolution.Dao.CpuSocketDao;
+import lk.bytetechsolution.Dao.CpuSuffixDao;
+import lk.bytetechsolution.Entity.CpuSocketEntity;
+import lk.bytetechsolution.Entity.CpuSuffixEntity;
+
+import java.util.*;
+
+
+
+/*
+    * implemented mapping to available for use  
+    * add implemented mapping to servelet container for use
+    * fundemental component for build REST full api  (representational state transfer)
+    * restcontroller tell spingboot to class/methods responsible to handle incoming http requests and produce appropiate http response(using json,xml format)
+*/
+@RestController
+public class CpuSuffixController {
+     /* 
+     * AutoWired used for automatic dependency injection
+     * inject cpusocketdao Instance into dao variable
+     * the method can use dao for save,retrive,maipulate employee data
+     */
+    @Autowired
+    private CpuSuffixDao dao;
+    
+
+     /* 
+     * define mapping for get all GEN data from database
+     * produce -> data return format(json,xml)
+     * value or path -> specify url pattern to which the method will be mapped
+     * List<cpuseries> -> return the list of Employee object
+     * 
+     * also you can use
+     * @requestMapping(value="/cpuseries/alldata",produces='application.json',method=RequestMethod.GET)
+     */
+    @GetMapping(value = "/cpusuffix/alldata", produces ="application/json" ) 
+    public List<CpuSuffixEntity> allEmployeeData() {
+
+        return dao.findAll();
+    }
+
+     //get cpu suffix according to brand
+    @GetMapping(value = "/cpusuffix/cpusocketbybrand/{brandname}",produces = "application/json")
+    public List<CpuSuffixEntity> GetSuffixByBrand(@PathVariable("brandname") String brandname){
+        return dao.getSuffixByBrand(brandname);
+    }
+
+     //get cpu suffix according to brand
+     @GetMapping(value = "/cpusuffix/cpusocketbycpuseries/{seriesname}",produces = "application/json")
+     public List<CpuSocketEntity> GetCpuSuffixByCpuSeries(@PathVariable("seriesname") String seriesname){
+         return dao.getSuffixBySeries(seriesname);
+     }
+}
