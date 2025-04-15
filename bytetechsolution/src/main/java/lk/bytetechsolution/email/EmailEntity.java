11d9cff3ca133035cfa520342e6536fe32462e53
Yasith <yasithnawodya9@gmail.com>
2025-04-16 00:12:57 +0530

    testing mail service add

---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/email/EmailEntity.java
@@ -0,0 +1,15 @@
+/* package lk.bytetechsolution.email;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class  EmailEntity{
+    private String sendto;
+    private String msgBody;
+    private String subject;
+    private String attachment;
+} */
---
+++ bytetechsolution/src/main/java/lk/bytetechsolution/email/EmailService.java
@@ -0,0 +1,16 @@
+/* package lk.bytetechsolution.email;
+
+import jakarta.mail.MessagingException;
+
+public interface EmailService {
+
+    //Mthod one
+    //send email without any attachment
+    void sendSimpleMail(EmailEntity email);
+
+    //method 2
+    //send email with attachment
+    void sendMailWithAttachment(EmailEntity email) throws MessagingException;
+
+} 
+ */
