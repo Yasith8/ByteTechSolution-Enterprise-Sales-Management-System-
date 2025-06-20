dea98b44c6c426e739bb2f75f045074f95a8569a
Yasith <yasithnawodya9@gmail.com>
2025-06-20 23:48:29 +0530

    serial no list dao changes to get new serial no list to accessories'

--- bytetechsolution/src/main/java/lk/bytetechsolution/Dao/SerialNoListDao.java
+++ bytetechsolution/src/main/java/lk/bytetechsolution/Dao/SerialNoListDao.java
@@ -1,6 +1,7 @@
 package lk.bytetechsolution.Dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import lk.bytetechsolution.Entity.SerialNoListEntity;
 
@@ -12,5 +13,6 @@ import lk.bytetechsolution.Entity.SerialNoListEntity;
  * can use for custom query methods
  */
 public interface SerialNoListDao extends JpaRepository<SerialNoListEntity,Integer>{
-    
+     @Query(value = "select concat('SNL',lpad(substring(max(sno.serialno),4)+1,4,'0')) as serialno from bytetechsolution.serial_no_list as sno",nativeQuery = true)
+    String getNextASCSeriealNo();
 }
